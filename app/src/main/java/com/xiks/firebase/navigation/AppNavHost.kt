8c04b20c5204270a5ff77d0ca019b51dfe494e36
senator423 <oumasenator@gmail.com>
2025-04-18 21:18:20 +0300

    first commit

---
+++ app/src/main/java/com/xiks/firebase/navigation/AppNavHost.kt
@@ -0,0 +1,54 @@
+package com.xiks.firebase.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.xiks.firebase.ui.theme.screens.Home.HomeScreen
+import com.xiks.firebase.ui.theme.screens.Login.LoginScreen
+import com.xiks.firebase.ui.theme.screens.Products.AddProductsScreen
+import com.xiks.firebase.ui.theme.screens.Products.UpdateProductsScreen
+import com.xiks.firebase.ui.theme.screens.Products.ViewProductsScreen
+import com.xiks.firebase.ui.theme.screens.Products.ViewUploadsScreen
+import com.xiks.firebase.ui.theme.screens.Register.RegisterScreen
+import com.xiks.login_registrationfirebase.ui.theme.screens.home.HomeScreen
+import com.xiks.login_registrationfirebase.ui.theme.screens.login.LoginScreen
+import com.xiks.login_registrationfirebase.ui.theme.screens.productus.AddProductsScreen
+import com.xiks.login_registrationfirebase.ui.theme.screens.productus.UpdateProductsScreen
+import com.xiks.login_registrationfirebase.ui.theme.screens.productus.ViewProductsScreen
+import com.xiks.login_registrationfirebase.ui.theme.screens.productus.ViewUploadsScreen
+import com.xiks.login_registrationfirebase.ui.theme.screens.register.RegisterScreen
+
+
+@Composable
+fun AppNavHost(modifier: Modifier=Modifier,navController:NavHostController= rememberNavController(),startDestination:String= ROUTE_LOGIN) {
+
+    NavHost(navController = navController, modifier=modifier, startDestination = startDestination ){
+        composable(ROUTE_LOGIN){
+            LoginScreen(navController)
+        }
+        composable(ROUTE_REGISTER){
+            RegisterScreen(navController)
+        }
+
+        composable(ROUTE_HOME){
+            HomeScreen(navController)
+        }
+        composable(ROUTE_ADD_PRODUCT) {
+            AddProductsScreen(navController)
+        }
+        composable(ROUTE_VIEW_PRODUCT){
+            ViewProductsScreen(navController)
+        }
+        composable(ROUTE_UPDATE_PRODUCT+ "/{id}"){passedData ->
+            UpdateProductsScreen(navController,passedData.arguments?.getString("id")!!)
+        }
+        composable(ROUTE_VIEW_UPLOAD){
+            ViewUploadsScreen(navController)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/xiks/firebase/ui/theme/screens/senator.kt
@@ -0,0 +1,2 @@
+package com.xiks.firebase.ui.theme.screens
+
