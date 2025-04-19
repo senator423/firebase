bb0331340e38d8a7fd73b6a68aa953385523d20c
senator423 <oumasenator@gmail.com>
2025-04-19 13:46:44 +0300

    first commit

--- app/src/main/java/com/xiks/firebase/ui/theme/screens/Login/login.screen.kt
+++ app/src/main/java/com/xiks/firebase/ui/theme/screens/Login/login.screen.kt
@@ -1,5 +1,9 @@
 package com.xiks.firebase.ui.theme.screens.Login
 
+//import androidx.compose.material.Button
+//import androidx.compose.material.OutlinedTextField
+//import androidx.compose.material.Text
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,12 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-//import androidx.compose.material.Button
-//import androidx.compose.material.OutlinedTextField
-//import androidx.compose.material.Text
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,8 +32,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.xiks.login_registrationfirebase.data.AuthViewModel
-import com.xiks.login_registrationfirebase.navigation.ROUTE_REGISTER
 import com.xiks.firebase.data.AuthViewModel
 import com.xiks.firebase.navigation.ROUTE_REGISTER
 
